b5303f1008ce964aff83da36d7d78c7b59e88a01
Sarah Weaver <lwflouisa@gmail.com>
2021-09-25 23:53:17 -0400

    Create standard.rb

---
+++ standard.rb
@@ -0,0 +1,29 @@
+# Standard brute force search.
+def standard_search
+  number = 0
+
+  options_list = File.read("options/option_list.txt").to_s.split(" ")
+
+  # Candidate files
+  candidate = File.read("candidate/algorithm_candidate.txt").strip.to_s
+
+  # Bases iteration limit on options list size.
+  size_limit = options_list.size.to_i
+
+  # Iterate size limit times comparing option with candidate.
+  size_limit.times do
+    option = options_list[number]
+
+    print "Candidate: #{candidate} Option: #{option}"
+
+    if option == candidate
+      puts " #{option} matches the candidate #{candidate}."
+
+      abort
+    else
+      puts " #{option} does not match the candidate #{candidate}."
+    end
+
+    sleep(3)
+  end
+end
